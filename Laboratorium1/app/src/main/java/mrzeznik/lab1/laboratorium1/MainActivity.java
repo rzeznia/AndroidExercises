6995af46bad89d29a47887307a98ddb4ef40f5e9
rzeznia <marcin.gd3@gmail.com>
2018-02-22 20:21:02 +0100

    v 1.1 Improvements: + UI improved + Exception avoid & Added Toast for divide by zero + Exception avoid when input data empty

--- Laboratorium1/app/src/main/java/mrzeznik/lab1/laboratorium1/MainActivity.java
+++ Laboratorium1/app/src/main/java/mrzeznik/lab1/laboratorium1/MainActivity.java
@@ -1,14 +1,18 @@
 package mrzeznik.lab1.laboratorium1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
-    TextView wynik; EditText liczba1; EditText liczba2; int temp1; int temp2;
+    TextView wynik; EditText liczba1; EditText liczba2; int temp1; int temp2; Toast zeroDiv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,28 +21,51 @@ public class MainActivity extends AppCompatActivity {
         liczba1=findViewById(R.id.liczba1EditText);
         liczba2=findViewById(R.id.liczba2EditText);
     }
+    public boolean checkValues()
+    {
+        if(TextUtils.isEmpty(liczba1.getText()) == true && TextUtils.isEmpty(liczba2.getText()) == true) {
+            Toast.makeText(getApplicationContext(), "Wprowadź wymagane dane wejściowe", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else
+        {
+            temp1 = Integer.parseInt(liczba1.getText().toString());
+            temp2 = Integer.parseInt(liczba2.getText().toString());
+            return true;
+        }
+    }
     public void dodawanieOnClick(View view)
     {
-        temp1 = Integer.parseInt(liczba1.getText().toString());
-        temp2 = Integer.parseInt(liczba2.getText().toString());
-        wynik.setText(String.valueOf(temp1+temp2));
+        if(checkValues() == true)
+            wynik.setText(String.valueOf(temp1 + temp2));
     }
     public void odejmowanieOnClick(View view)
     {
-        temp1 = Integer.parseInt(liczba1.getText().toString());
-        temp2 = Integer.parseInt(liczba2.getText().toString());
-        wynik.setText(String.valueOf(temp1-temp2));
+        if(checkValues() == true)
+            wynik.setText(String.valueOf(temp1 - temp2));
     }
     public void mnozenieOnClick(View view)
     {
-        temp1 = Integer.parseInt(liczba1.getText().toString());
-        temp2 = Integer.parseInt(liczba2.getText().toString());
-        wynik.setText(String.valueOf(temp1*temp2));
+        if(checkValues() == true)
+            wynik.setText(String.valueOf(temp1 * temp2));
     }
     public void dzielenieOnClick(View view)
     {
-        temp1 = Integer.parseInt(liczba1.getText().toString());
-        temp2 = Integer.parseInt(liczba2.getText().toString());
-        wynik.setText(String.valueOf(temp1/temp2));
+        if(checkValues() == true)
+        {
+            if (temp2 == 0) {
+                Toast.makeText(getApplicationContext(), "Nie można dzielić przez zero!", Toast.LENGTH_SHORT).show();
+                wynik.setText("Error!");
+            }
+            else
+                wynik.setText(String.valueOf(temp1 / temp2));
+
+        }
+    }
+    public void clrBtnOnClick(View view)
+    {
+        liczba1.setText(null);
+        liczba2.setText(null);
+        wynik.setText("0");
     }
 }
